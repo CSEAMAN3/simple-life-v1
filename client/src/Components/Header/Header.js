ab207e73f5b9b412188160a81350176043c23640
Chris Seaman <chris@TechEd-0014.techeducators.co.uk>
2023-03-10 16:26:28 +0000

    updated

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -1,6 +1,10 @@
 import "./Header.css";
 import React from "react";
 
+import { Link } from "react-router-dom";
+
+import Burger from "../Burger/Burger";
+
 export default function Header() {
   return (
     <header className="header">
@@ -9,6 +13,15 @@ export default function Header() {
           <h1 className="logo">The Simple Life</h1>
           <span className="logo-strapline">A guide to minimilist living</span>
         </div>
+        <Burger />
+        <nav className="header-nav">
+          <Link to="/" className="header-nav-link">
+            <li className="header-nav-list-item">Home</li>
+          </Link>
+          <Link to="/about" className="header-nav-link">
+            <li className="header-nav-list-item">About</li>
+          </Link>
+        </nav>
       </div>
     </header>
   );
--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -1,12 +1,21 @@
 import "./Home.css";
 import React from "react";
 
+import { Helmet } from "react-helmet-async";
+
 export default function Home() {
   return (
-    <main>
-      <div className="home-main-container container">
-        <h2>This is the Home page.</h2>
-      </div>
-    </main>
+    <>
+      <Helmet>
+        <title>Air bnb homepage</title>
+        <meta name="description" content="This is the main homepage for the air bnb clone react site." />
+        <link rel="canonical" href="/" />
+      </Helmet>
+      <main>
+        <div className="home-main-container container">
+          <h2>This is the Home page.</h2>
+        </div>
+      </main>
+    </>
   );
 }
